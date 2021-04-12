47780ef61fe700d2e771435c0464c6e574d1a10c
Michael Pingel <mdpingel@gmail.com>
2021-04-12 13:31:37 -0700

    pass test coinCounter() with dimes, nickels, and pennies
    Co-authored-by: Daniel Adeyemi <adeyemidany@gmail.com>

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -5,25 +5,25 @@ import {
 describe('coinCounter', () => {
   test("try to return something", () => {
     const result = coinCounter(0);
-    expect(result).toEqual('Nickels: 0, Pennies: 0');
+    expect(result).toEqual('Dimes: 0, Nickels: 0, Pennies: 0');
   });
 
   test('should return the number of pennies', () => {
     const result = coinCounter(0.04);
-    expect(result).toEqual('Nickels: 0, Pennies: 4');
+    expect(result).toEqual('Dimes: 0, Nickels: 0, Pennies: 4');
   });
 
   test('should return the number of nickels', () => {
     const result = coinCounter(0.05);
-    expect(result).toEqual('Nickels: 1, Pennies: 0');
+    expect(result).toEqual('Dimes: 0, Nickels: 1, Pennies: 0');
   });
 
   test('should return the number of nickels and pennies', () => {
     const result = coinCounter(0.09);
-    expect(result).toEqual('Nickels: 1, Pennies: 4');
+    expect(result).toEqual('Dimes: 0, Nickels: 1, Pennies: 4');
   });
-  test('should return the number of dimes', () => {
-    const result = coinCounter(.1);
-    expect(result).toEqual('Dimes: 1, Nickels: 0, Pennies: 0');
+  test('should return the number of dimes, nickels, and pennies', () => {
+    const result = coinCounter(0.19);
+    expect(result).toEqual('Dimes: 1, Nickels: 1, Pennies: 4');
   });
 });
--- src/main.js
+++ src/main.js
@@ -6,8 +6,9 @@
 export function coinCounter(change) {
   // const cents = (isNaN(change)) ? 0 : (change * 100);
   const cents = change * 100;
-  const nickels = parseInt(cents / 5);
+  const dimes = parseInt(cents / 10);
+  const nickels = parseInt(cents % 10 / 5);
   const pennies = cents % 5;
 
-  return `Nickels: ${nickels}, Pennies: ${pennies}`;
+  return `Dimes: ${dimes}, Nickels: ${nickels}, Pennies: ${pennies}`;
 }
