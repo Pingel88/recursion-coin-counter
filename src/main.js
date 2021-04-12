7a2631e53502dc87157556ec650e06161a042dbf
Michael Pingel <mdpingel@gmail.com>
2021-04-12 13:19:13 -0700

    refactor test coinCounter() with nickels and pennies
    Co-authored-by: Daniel Adeyemi <adeyemidany@gmail.com>

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ export function coinCounter(change) {
   // const cents = (isNaN(change)) ? 0 : (change * 100);
   const cents = change * 100;
   const nickels = parseInt(cents / 5);
-  const pennies = cents - nickels * 5;
+  const pennies = cents % 5;
 
   return `Nickels: ${nickels}, Pennies: ${pennies}`;
 }
