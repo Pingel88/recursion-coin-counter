57413aebc5ed98038ea9fab38e91ac8daeb6134e
Michael Pingel <mdpingel@gmail.com>
2021-04-12 11:10:10 -0700

    write coinCounter() test to return number of nickels and pennies
    Co-authored-by: Daniel Adeyemi <adeyemidany@gmail.com>

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -3,27 +3,31 @@ import {
 } from '../src/main.js';
 
 describe('coinCounter', () => {
-  test("try to return something", () => {
-    const change = 0;
-    expect(coinCounter(change)).toEqual(0);
-  });
+      test("try to return something", () => {
+        const result = coinCounter(0);
+        expect(coinCounter(result)).toEqual('Nickels: 0, Pennies: 0');
+      });
 
-  test('should return number of pennies', () => {
-    const change = 0.04;
-    expect(coinCounter(change)).toEqual(4);
-  });
+      test('should return the number of pennies', () => {
+        const result = coinCounter(0.04);
+        expect(coinCounter(result)).toEqual('Nickels: 0, Pennies: 4');
+      });
 
-  test('should return number of nickels', () => {
-    const change = 0.05;
-    expect(coinCounter(change)).toEqual(1);
-  });
-})
+      test('should return the number of nickels', () => {
+        const result = coinCounter(0.05);
+        expect(result).toEqual('Nickels: 1, Pennies: 0');
+      });
+
+      test('should return the number of nickels and pennies', () => {
+        const result = coinCounter(0.09);
+        expect(result).toEqual('Nickels: 1, Pennies: 4')
+      })
 
 
-// describe('Age', () => {
-//   test('should correctly create an Age object with age and expectancy for male', () => {
-//     const age = new Age(34, 'male');
-//     expect(age.years).toEqual(34);
-//     expect(age.expect).toEqual(42);
-//   });
-// });
+      // describe('Age', () => {
+      //   test('should correctly create an Age object with age and expectancy for male', () => {
+      //     const age = new Age(34, 'male');
+      //     expect(age.years).toEqual(34);
+      //     expect(age.expect).toEqual(42);
+      //   });
+      // });
