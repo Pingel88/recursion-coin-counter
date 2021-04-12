b3ee63f687a974bd9f988bb10eca1ea2ecba6585
Michael Pingel <mdpingel@gmail.com>
2021-04-12 12:04:42 -0700

    modify tests to expect a string and get a good fail
    Co-authored-by: Daniel Adeyemi <adeyemidany@gmail.com>

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -5,12 +5,12 @@ import {
 describe('coinCounter', () => {
   test("try to return something", () => {
     const result = coinCounter(0);
-    expect(coinCounter(result)).toEqual('Nickels: 0, Pennies: 0');
+    expect(result).toEqual('Nickels: 0, Pennies: 0');
   });
 
   test('should return the number of pennies', () => {
     const result = coinCounter(0.04);
-    expect(coinCounter(result)).toEqual('Nickels: 0, Pennies: 4');
+    expect(result).toEqual('Nickels: 0, Pennies: 4');
   });
 
   test('should return the number of nickels', () => {
@@ -23,12 +23,3 @@ describe('coinCounter', () => {
     expect(result).toEqual('Nickels: 1, Pennies: 4');
   });
 });
-
-
-// describe('Age', () => {
-//   test('should correctly create an Age object with age and expectancy for male', () => {
-//     const age = new Age(34, 'male');
-//     expect(age.years).toEqual(34);
-//     expect(age.expect).toEqual(42);
-//   });
-// });
--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@
 // import './css/styles.css';
 
 export function coinCounter(change) {
-
+  // const cents = (isNaN(change)) ? 0 : (change * 100);
   const cents = change * 100;
   const nickels = cents / 5;
-  return (cents < 5) ? cents : nickels;
+
+  return `Nickels: ${nickels}, Pennies: ${cents}`;
 }
